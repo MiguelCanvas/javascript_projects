bc96b17dec12b578817ee4c96a1c01adf7146da1
Miguel Vasquez <miguelvasq3z@gmail.com>
2022-08-16 17:08:29 -0500

    Setting up the second project - Counter

--- project_1/script.js
+++ project_1/script.js
@@ -3,6 +3,7 @@ const buttonEl = document.getElementById("button-el")
 let hex = "#"
 let hexaArray = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
+
 buttonEl.addEventListener("click", () =>{
 
     for(let i = 0; i<6;i++){
